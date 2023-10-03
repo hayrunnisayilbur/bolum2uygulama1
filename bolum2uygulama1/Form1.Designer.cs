1c76cf491265bcf075da8ef7f424eabab8539368
hayrunnisayilbur <145451185+hayrunnisayilbur@users.noreply.github.com>
2023-10-03 14:55:27 +0300

    Add files via upload

---
+++ bolum2uygulama1/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace bolum2uygulama1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMesaj = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnMesaj
+            // 
+            this.btnMesaj.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnMesaj.Location = new System.Drawing.Point(114, 101);
+            this.btnMesaj.Name = "btnMesaj";
+            this.btnMesaj.Size = new System.Drawing.Size(241, 136);
+            this.btnMesaj.TabIndex = 0;
+            this.btnMesaj.Text = "mesaj ver";
+            this.btnMesaj.UseVisualStyleBackColor = true;
+            this.btnMesaj.Click += new System.EventHandler(this.btnMesaj_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnMesaj);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMesaj;
+    }
+}
+
---
+++ bolum2uygulama1/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bolum2uygulama1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMesaj_Click(object sender, EventArgs e)
+        {
+            int yas = 14;
+
+            if (yas >= 18) { 
+               MessageBox.Show("ehliyet alabilirsiniz");
+            }
+
+            if (yas < 18) 
+
+                    MessageBox.Show("üzgünüm biraz daha büyümelisin");
+           
+            {
+
+            }
+            {
+
+
+            }
+        }
+    }
+}
